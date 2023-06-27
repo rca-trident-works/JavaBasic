cda665c9459ec9583ff485b38b2e44b9d094513d
ろむねこ <66072112+r-ca@users.noreply.github.com>
2023-06-27 12:02:59 +0900

    0305

---
+++ JavaBasic/src/pkg0305/LogicalSumExample.java
@@ -0,0 +1,13 @@
+package pkg0305;
+
+public class LogicalSumExample {
+    public static void main(String[] args) {
+        int age;
+        age = 20;
+        if (age < 13 || age >= 65) {
+            System.out.println("入場料は無料です。");
+        } else {
+            System.out.println("料金が必要です。");
+        }
+    }
+}
