2d3c55f1934b5f8b62ab5081e9eb34d335bfed15
ろむねこ <66072112+r-ca@users.noreply.github.com>
2023-06-27 11:38:06 +0900

    pkg0407

---
+++ JavaBasic/src/pkg0407/Multiply.java
@@ -0,0 +1,9 @@
+package pkg0407;
+
+public class Multiply {
+    public static void main(String[] args) {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        System.out.println(a * b);
+    }
+}
