7a5fef86b656ea1e3466d0ae70a811a7d19cb275
ろむねこ <66072112+r-ca@users.noreply.github.com>
2023-06-27 12:03:09 +0900

    0303

---
+++ JavaBasic/src/pkg0303/ElseExample.java
@@ -0,0 +1,15 @@
+package pkg0303;
+
+public class ElseExample {
+    public static void main(String[] args) {
+        int age;
+        age = 20;
+        if (age < 4) {
+            System.out.println("入場料は無料です。");
+        } else if (age < 13) {
+            System.out.println("子供料金で入場できます。");
+        } else {
+            System.out.println("大人料金が必要です。");
+        }
+    }
+}
