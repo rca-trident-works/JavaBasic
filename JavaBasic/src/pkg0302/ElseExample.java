6a062c2e39e2fd7775470d0e0f7fbcfd84694b4a
ろむねこ <66072112+r-ca@users.noreply.github.com>
2023-06-27 12:03:13 +0900

    0302

---
+++ JavaBasic/src/pkg0302/ElseExample.java
@@ -0,0 +1,13 @@
+package pkg0302;
+
+public class ElseExample {
+    public static void main(String[] args) {
+        int age;
+        age = 20;
+        if (age < 20) {
+            System.out.println("未成年ですね");
+        } else {
+            System.out.println("二十歳以上ですね");
+        }
+    }
+}
