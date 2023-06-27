72521415693fba0af40a43dec2ed5f4ec82874a7
ろむねこ <66072112+r-ca@users.noreply.github.com>
2023-06-27 11:38:10 +0900

    pkg0408

---
+++ JavaBasic/src/pkg0408/ReturnExample.java
@@ -0,0 +1,13 @@
+package pkg0408;
+
+public class ReturnExample {
+    public static double getAreaOfCircle(double radius) {
+        return radius * radius * 3.14;
+    }
+
+    public static void main(String[] args) {
+        double circleArea = getAreaOfCircle(2.5);
+        System.out.println("半径2.5の円の面積は" + circleArea);
+    }
+
+}
