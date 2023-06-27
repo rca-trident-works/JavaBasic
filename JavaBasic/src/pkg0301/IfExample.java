660dd0d130f1137220da8b5352a0890b2a961556
ろむねこ <66072112+r-ca@users.noreply.github.com>
2023-06-27 12:03:19 +0900

    0301

---
+++ JavaBasic/src/pkg0301/IfExample.java
@@ -0,0 +1,11 @@
+package pkg0301;
+
+public class IfExample {
+    public static void main(String[] args) {
+        int age;
+        age = 18;
+        if (age < 20) {
+            System.out.println("未成年ですね");
+        }
+    }
+}
